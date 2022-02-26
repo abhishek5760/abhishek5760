872a97b032b742e0e767c38ca2f2f8fc74d1cb41
abhishek5760 <77285056+abhishek5760@users.noreply.github.com>
2022-02-26 14:02:44 +0530

    Create sum ofn.java

---
+++ Experiment 1/sum ofn.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ class sumscan
+ {
+       public static void main(String args[])
+       {
+         Scanner in= new Scanner(System.in);
+         System.out.println("Enter the number");
+         int n=in.nextInt();
+         int sum=0;
+         System.out.println("Sum of first"+n+"=");
+            for(int i=1;i<=10;i++)
+               {
+                sum=sum+i;
+                 }
+        System.out.println(sum3);
+       }
+ }
